13685ab197ea96fc4780758ce3427335fc7d1fe5
HuafengLau <727860046@qq.com>
2014-03-24 23:05:50 +0800

    delete test  qqlog url
    rt

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('account.views',
     url(r'^QQlogin/$', 'QQ_login', name='account_QQ_login'),
-    url(r'^(?P<openid>.+)/$', 'testQQlog', name='account_QQ_testQQlog'),
 )
