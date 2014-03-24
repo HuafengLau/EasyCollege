450bc9354a84cdf461f0db38fa11bf9e6172aa3f
HuafengLau <727860046@qq.com>
2014-03-24 23:40:13 +0800

    test qq log
    rt

--- account/views.py
+++ account/views.py
@@ -136,8 +136,10 @@ def fangqiu(request):
             )
             this_AuthorLog.save()  
 
-        username = '3F668C02ABF661C7306F9C4E6EB4053E@qq1.com'
-        password = '123'
+        #username = '3F668C02ABF661C7306F9C4E6EB4053E@qq1.com'
+        #password = '123'
+        username='you_xiang_123@sina.com'
+        password='lhf564495'
         user = authenticate(username=username, password=password)
         login(request, user)
 
