de11f52aaf544578283baa41dc4eb22113fbd944
HuafengLau <727860046@qq.com>
2014-03-24 23:17:38 +0800

    add test url
    rt for qqlog

--- account/urls.py
+++ account/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('account.views',
     url(r'^QQlogin/$', 'QQ_login', name='account_QQ_login'),
+    url(r'^(?P<openid>.+)/$', 'testQQlog', name='account_QQ_testQQlog'),
 )
