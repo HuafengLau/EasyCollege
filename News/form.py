5aeceec9398a3dec308f47b823df48307a503536
HuafengLau <727860046@qq.com>
2014-04-03 11:16:52 +0800

    fix submitNews bug
    rt and remove explore link

--- News/form.py
+++ News/form.py
@@ -46,7 +46,7 @@ class LinkNewsForm3(forms.ModelForm):
     
     class Meta:
         model = News
-        exclude = ('ups','downs','time','gold','score','controversy','hot','user','text','pic','comment_num','mp3','open','secret','read')  
+        exclude = ('ups','downs','time','gold','score','controversy','hot','user','text','pic','comment_num','mp3','open','secret','read','agree_man')  
  
 class LinkNewsForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):  
@@ -100,7 +100,7 @@ class LinkNewsForm(forms.ModelForm):
     
     class Meta:
         model = News
-        exclude = ('ups','downs','time','gold','score','controversy','hot','user','text','pic','comment_num','mp3','open','secret','read') 
+        exclude = ('ups','downs','time','gold','score','controversy','hot','user','text','pic','comment_num','mp3','open','secret','read','agree_man') 
 
 class TextNewsForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):  
@@ -141,7 +141,7 @@ class TextNewsForm(forms.ModelForm):
     
     class Meta:
         model = News
-        exclude = ('ups','downs','time','gold','score','controversy','hot','user','link','pic','comment_num','mp3','open','secret','read')
+        exclude = ('ups','downs','time','gold','score','controversy','hot','user','link','pic','comment_num','mp3','open','secret','read','agree_man')
 
 class PicNewsForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):  
@@ -183,7 +183,7 @@ class PicNewsForm(forms.ModelForm):
     
     class Meta:
         model = News
-        exclude = ('ups','downs','time','gold','score','controversy','hot','user','link','text','comment_num','mp3','open','secret','pic','read')  
+        exclude = ('ups','downs','time','gold','score','controversy','hot','user','link','text','comment_num','mp3','open','secret','pic','read','agree_man')  
         
 class mp3NewsForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):  
@@ -225,5 +225,5 @@ class mp3NewsForm(forms.ModelForm):
     
     class Meta:
         model = News
-        exclude = ('ups','downs','time','gold','score','controversy','hot','user','link','text','comment_num','pic','open','secret','mp3','read') 
+        exclude = ('ups','downs','time','gold','score','controversy','hot','user','link','text','comment_num','pic','open','secret','mp3','read','agree_man') 
         
